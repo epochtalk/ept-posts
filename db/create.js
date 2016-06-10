d1d34b7737c80158dcc275f671bdebb1815c51d2
Edward Kim <ed@slickage.com>
2016-06-10 13:36:26 -1000

    Updated posts.create to set deleted and locked
    The posts.create method can now set the deleted and locked flag on
    creation.

--- db/create.js
+++ db/create.js
@@ -8,9 +8,12 @@ var helper = dbc.helper;
 
 module.exports = function(post) {
   post = helper.deslugify(post);
+  post.deleted = post.deleted || false;
+  post.locked = post.locked || false;
+
   var q, params;
-  q = 'INSERT INTO posts(thread_id, user_id, title, body, raw_body, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, now(), now()) RETURNING id, created_at';
-  params = [post.thread_id, post.user_id, post.title, post.body, post.raw_body];
+  q = 'INSERT INTO posts(thread_id, user_id, title, body, raw_body, deleted, locked, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, now(), now()) RETURNING id, created_at';
+  params = [post.thread_id, post.user_id, post.title, post.body, post.raw_body, post.deleted, post.locked];
   return using(db.createTransaction(), function(client) {
     return client.queryAsync(q, params)
     .then(function(results) {
