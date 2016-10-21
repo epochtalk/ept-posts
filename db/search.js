680431d38b73bdf0c769ce921dcfffd05ce0d875
James Wang <jw@slickage.com>
2016-10-20 17:29:17 -1000

    Post position for search item link to thread

--- db/search.js
+++ db/search.js
@@ -13,7 +13,7 @@ module.exports = function(opts) {
   var search = opts.searchStr;
   var result = {};
   // var q = 'SELECT p.*, (SELECT username from users where id = p.user_id) as username, (SELECT p2.title FROM posts p2 WHERE p2.thread_id = p.thread_id ORDER BY created_at LIMIT 1) as thread_title, b.id as board_id, b.name as board_name from posts p LEFT JOIN boards b ON ((SELECT t.board_id FROM threads t WHERE p.thread_id = t.id)  = b.id) WHERE body LIKE \'% ' + querystring.unescape(search) + ' %\' ORDER BY p.created_at DESC LIMIT $1 OFFSET $2';
-  var q = 'SELECT p.id, p.title, p.user_id, u.username, p.thread_id, p.body, t.board_id, b.name AS board_name FROM posts p, users u, threads t, boards b, to_tsquery(\'simple\', $1) AS q WHERE (p.tsv @@ q) AND (p.user_id = u.id) AND (p.thread_id = t.id) AND (t.board_id = b.id) ORDER BY p.created_at DESC LIMIT $2 OFFSET $3';
+  var q = 'SELECT p.id, p.title, p.user_id, u.username, p.thread_id, p.position, p.body, t.board_id, b.name AS board_name FROM posts p, users u, threads t, boards b, to_tsquery(\'simple\', $1) AS q WHERE (p.tsv @@ q) AND (p.user_id = u.id) AND (p.thread_id = t.id) AND (t.board_id = b.id) ORDER BY p.created_at DESC LIMIT $2 OFFSET $3';
   var params = [querystring.unescape(search), limit, offset];
     if (!search) {
       q = 'SELECT LIMIT 0';
