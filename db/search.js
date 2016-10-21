f28d2b088737e9f8c00e6b49e3ecb0260f64b055
James Wang <jw@slickage.com>
2016-10-20 16:47:27 -1000

    TSV search query sans paging

--- db/search.js
+++ db/search.js
@@ -12,8 +12,9 @@ module.exports = function(opts) {
   var order = opts.sortDesc ? 'DESC' : 'ASC';
   var search = opts.searchStr;
   var result = {};
-  var q = 'SELECT p.*, (SELECT username from users where id = p.user_id) as username, (SELECT p2.title FROM posts p2 WHERE p2.thread_id = p.thread_id ORDER BY created_at LIMIT 1) as thread_title, b.id as board_id, b.name as board_name from posts p LEFT JOIN boards b ON ((SELECT t.board_id FROM threads t WHERE p.thread_id = t.id)  = b.id) WHERE body LIKE \'% ' + querystring.unescape(search) + ' %\' ORDER BY p.created_at DESC LIMIT $1 OFFSET $2';
-  var params = [limit, offset];
+  // var q = 'SELECT p.*, (SELECT username from users where id = p.user_id) as username, (SELECT p2.title FROM posts p2 WHERE p2.thread_id = p.thread_id ORDER BY created_at LIMIT 1) as thread_title, b.id as board_id, b.name as board_name from posts p LEFT JOIN boards b ON ((SELECT t.board_id FROM threads t WHERE p.thread_id = t.id)  = b.id) WHERE body LIKE \'% ' + querystring.unescape(search) + ' %\' ORDER BY p.created_at DESC LIMIT $1 OFFSET $2';
+  var q = 'SELECT p.id, p.title, p.user_id, u.username, p.thread_id, p.body, t.board_id, b.name AS board_name FROM posts p, users u, threads t, boards b, to_tsquery(\'simple\', $1) AS q WHERE (p.tsv @@ q) AND (p.user_id = u.id) AND (p.thread_id = t.id) AND (t.board_id = b.id) ORDER BY p.created_at DESC LIMIT $2 OFFSET $3';
+  var params = [querystring.unescape(search), limit, offset];
     if (!search) {
       q = 'SELECT LIMIT 0';
       params = undefined;
@@ -29,6 +30,7 @@ module.exports = function(opts) {
         var find = escapeRegExp(' ' + querystring.unescape(opts.searchStr) + ' ');
         var re = new RegExp(find, 'g');
         item.body = item.body.replace(re, '<div class="highlighted"> ' + opts.searchStr + ' </div>');
+        item.thread_title = item.title;
         return item;
       });
     }
